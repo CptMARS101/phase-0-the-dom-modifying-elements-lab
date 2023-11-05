eaa1b48cad01adc5fa3e4d9522302f4d590944db
Marcus Kitchen <marcusdkitchen@gmail.com>
2023-11-05 04:14:40 -0500

    DomMODLAB

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const mainElement = document.getElementById(`main`);
+const newHeader = document.createElement(`h1`);
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = 'Marcus is the champion';
+mainElement.parentNode.replaceChild(newHeader, mainElement);
